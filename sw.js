57b8cb116d71607668b1455577755819c76bef0e
guilledpn <g.delpedregal@gmail.com>
2026-07-02 03:21:09 -0400

    Bump PWA shell cache for v68 manifest

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION='crm-llamados-github-wrapper-v67';
+const APP_VERSION='crm-llamados-github-wrapper-v68';
 const APP_CACHE=APP_VERSION+'-shell';
 const SHELL_ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon.svg','./assets/logo-horizontal.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(APP_CACHE).then(cache=>cache.addAll(SHELL_ASSETS)).catch(()=>null));});
