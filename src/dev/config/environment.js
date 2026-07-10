0aa0faf8f9865dcd62d5256bce9c9cdf0c1c47ee
guilledpn <g.delpedregal@gmail.com>
2026-07-10 02:12:55 -0400

    dev: add isolated runtime environment module

---
+++ src/dev/config/environment.js
@@ -0,0 +1,41 @@
+(function bootstrapEnvironment(global) {
+  'use strict';
+
+  const namespace = global.AppDev = global.AppDev || {};
+  const environment = Object.freeze({
+    appEnv: 'DEV',
+    appName: 'APP LLAMADOS DEV',
+    appVersion: 'v1.05',
+    buildId: '__APP_BUILD__',
+    supabaseProject: 'crm-ffvv-dev',
+    supabaseProjectRef: 'xcujixexjbuqqzlbomgw',
+    supabaseUrl: 'https://xcujixexjbuqqzlbomgw.supabase.co',
+    supabasePublishableKey: '__DEV_SUPABASE_PUBLISHABLE_KEY__',
+    pageSize: 50,
+    contactsCacheVersion: 'v2_space_safe_2026_07_restore_v62',
+    storageNamespace: 'crm_ffvv_dev_',
+    legacySprintKey: 'recovery_crm_dev_20260709_sprint_v2',
+    macroDroidUrl: '',
+    tasksWebhookUrl: '',
+    externalIntegrationsEnabledByDefault: false,
+    serviceWorkerUrl: './sw.js',
+    serviceWorkerScope: './'
+  });
+
+  if (environment.appEnv !== 'DEV' || environment.supabaseProjectRef !== 'xcujixexjbuqqzlbomgw') {
+    throw new Error('Bloqueo de seguridad: configuración DEV inválida');
+  }
+  if (environment.supabaseUrl.indexOf('lijibbhpyyptodneafdd') !== -1) {
+    throw new Error('Bloqueo de seguridad: DEV contiene el endpoint de PROD');
+  }
+  if (global.location && !global.location.pathname.includes('/dev/')) {
+    throw new Error('Bloqueo de seguridad: la aplicación DEV está fuera de /dev/');
+  }
+
+  Object.defineProperty(namespace, 'environment', {
+    value: environment,
+    enumerable: true,
+    configurable: false,
+    writable: false
+  });
+})(window);
